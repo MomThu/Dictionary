b8aeb7a2d2b730cf8475c87c7dc4e984a74a9950
MomThu <64343076+MomThu@users.noreply.github.com>
2020-10-05 09:41:09 +0700

    commandline 2+3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         DictionaryCommandline test = new DictionaryCommandline();
-        test.dictionaryBasic();
+        test.dictionaryAdvanced();
+        test.dictionaryExportToFile();
     }
 }
